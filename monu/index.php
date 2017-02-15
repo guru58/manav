199df4bd4448906c897f9664319498519405ab6b
Deep Gagan <Deep Gagan>
2017-02-15 10:34:35 +0530

    new folder commited by guru

---
+++ monu/index.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>ManavElectricals</title>
+
+                                <!-- CSS SECTION START HERE  -->
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/owl.carousel.css" />
+    <link rel="stylesheet" href="css/owl.theme.css" />
+    <link rel="stylesheet" href="css/owl.transitions.css" />
+    <link rel="stylesheet" href="css/animate.css" />
+    <link href="https://fonts.googleapis.com/css?family=Philosopher|Old+Standard+TT|Lobster+Two|Abhaya+Libre" rel="stylesheet" />
+    <link rel="stylesheet" href="css/hover.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <style>
+        #owl-demo .item{
+            position: relative;
+        }
+        #owl-demo .item img{
+            display: block;
+            width: 100%;
+            height:662px;
+            
+        }
+    </style>
+                                
+                                <!-- CSS SECTION END HERE -->
+
+</head>
+<body>
+ 
+                        <!-- HEADER START HERE  -->
+
+
+                        <!-- CAROUSEL START HERE  -->
+    
+
+    <div id="owl-demo" class="owl-carousel owl-theme">
+        <div class="item">
+            <img class="img-responsive" src="https://www.walldevil.com/wallpapers/a66/backgrounds-twitter-retouch-color-industrial-electrical-computer.jpg" alt="ManavElectricals">
+        </div>
+        <div class="item">
+            <img class="img-responsive" src="http://wallpapercave.com/wp/wp1837609.jpg" alt="ManavElectricals">
+        </div>
+    </div>
+   
+    
+                            <!-- CAROUSEL END HERE -->
+
+    
+                        <!-- MENU SECTION START -->
+
+    <nav class="navbar" data-spy="affix" data-offset-top="670">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>                        
+                </button>
+                <a class="navbar-brand" href="#">Manav Electricals</a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a  class="hvr-underline-from-center " href="#">Home</a></li>
+                    <li><a  class="hvr-underline-from-center " href="#">About</a></li>
+                    <li><a  class="hvr-underline-from-center " href="#">Services</a></li>
+                    <li><a  class="hvr-underline-from-center " href="#">Products</a></li>
+                    <li><a  class="hvr-underline-from-center " href="#">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>  
+                        <!-- MENU SECTION END  -->
+
+                        <!-- HEADER END HERE -->
+
+                        <!-- ABOUT SECION START -->
+
+
+<div id="rowAbout">
+    <div class="row">
+        <div class="container">
+            <div class="aboutPhoto col-sm-6 col-md-6">
+                <img class="img-responsive" src="http://www.amdelectrics.co.uk/wp-content/uploads/electrician3-700x300.jpg" alt="">
+            </div>
+            <div class="aboutInfo col-sm-6 col-md-6">
+                <h1 class="wow fadeInRight">About Us</h1>
+                <hr/>
+                <p class="wow fadeInRight">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ipsum iusto neque assumenda earum! Soluta hic voluptatum, rerum, molestias dolorem sapiente assumenda laborum accusamus cupiditate iusto labore possimus minus eligendi.    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt optio quasi quo veniam ea excepturi eum voluptate iusto voluptates esse. Ratione iure quo dolorum, est possimus illo soluta fugiat accusantium?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta sunt cum reiciendis laudantium placeat et, quisquam qui rem consectetur exercitationem, voluptas quam blanditiis assumenda. Quas quidem a voluptate ullam excepturi?</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+                     <!-- ABOUT SECTION END -->
+
+<div id="rowForm">
+    <div class="rowShadow">
+    </div>
+    <div class="row">
+        <div class="container">
+            <div class="requestForm col-sm-12 col-md-12">
+            <h1>Request Service</h1><hr>
+                <form class="form-inline">
+
+                    <div class="form-group">
+                        <!-- <label for="name">Name:</label> -->
+                        <input type="name" class="form-control" id="name" placeholder="Enter name">
+                    </div>
+                    &nbsp;&nbsp;&nbsp;
+                    <div class="form-group">
+                        <!-- <label for="email">Email:</label> -->
+                        <input type="email" class="form-control" id="email" placeholder="Enter email">
+                    </div>
+                    &nbsp;&nbsp;&nbsp;
+                    <div class="form-group">
+                        <!-- <label for="pwd">Contact No.:</label> -->
+                        <input type="password" class="form-control" id="pwd" placeholder="Enter contact no.">
+                    </div>
+                   &nbsp;&nbsp;&nbsp;
+                    <div class="form-group">
+                        <!-- <label for="pwd">Contact No.:</label> -->
+                    <select name="" id="" class="form-control">
+                        <option value="commerical services">Commercial Services</option>
+
+                        <option value="commerical services">Industrial Services</option>
+
+                        <option value="commerical services">Residetal Services</option>
+
+                        <option value="commerical services">Electrical Services</option>
+                    </select>
+                    </div>
+                    &nbsp;&nbsp;&nbsp;
+                    <button type="submit" class="btn btn-info">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+ <div style="height:200px;margin-top:100px;">
+    	
+    </div>
+
+
+                                <!-- SCRIPT START HERE  -->
+
+<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+
+
+                           <!-- embedding owl carousel script -->
+
+<script src="js/owl.carousel.js"></script>
+<script>
+    $(document).ready(function() {
+        $("#owl-demo").owlCarousel({
+        navigation : false,
+        paginationSpeed : 300,
+        pagination:false,
+        singleItem:true,
+        transitionStyle : "fade",
+        autoPlay:true,
+        });
+      });
+</script>
+ 
+                            <!-- owl carousel script end here -->
+
+
+
+
+                            <!-- wow.js script start here -->
+
+
+<script src="js/wow.min.js"></script>
+  <script>
+    wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:100,
+        callback:function(box) {
+        console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+        }
+      }
+    );
+    wow.init();
+  </script>
+
+                           <!-- wow.js script end here -->
+
+                                <!-- SCRIPT END HERE -->
+
+
+</body>
+</html>
